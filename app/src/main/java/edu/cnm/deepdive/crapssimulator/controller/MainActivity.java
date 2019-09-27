8ca05596ae5e28f65e3245df786052ce2defc701
Sami <samimheard@gmail.com>
2019-09-27 14:40:00 -0600

    added functionality for rotating and leave in bg

--- app/src/main/java/edu/cnm/deepdive/crapssimulator/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/crapssimulator/controller/MainActivity.java
@@ -7,35 +7,44 @@ import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Round;
 import edu.cnm.deepdive.crapssimulator.R;
 import edu.cnm.deepdive.crapssimulator.view.RoundAdapter;
+import edu.cnm.deepdive.crapssimulator.viewmodel.MainViewModel;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-  private Game game;
-  private Random rng;
+  private MainViewModel viewModel;
   private RoundAdapter adapter;
   private TextView tally;
-  private ListView rolls;
   private boolean running;
-
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    setupUI();
+    setupViewModel();
+  }
+
+  private void setupViewModel() {
+    viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+    viewModel.getGame().observe(this, (game) -> updateTally(game));
+    viewModel.getRound().observe(this, (round) -> updateRolls(round));
+    viewModel.isRunning().observe(this, (running) -> {
+      this.running = running;
+      invalidateOptionsMenu();
+    });
+  }
+
+  private void setupUI() {
     tally = findViewById(R.id.tally);
-    rolls = findViewById(R.id.rolls);
+    ListView rolls = findViewById(R.id.rolls);
     adapter = new RoundAdapter(this);
     rolls.setAdapter(adapter);
-    rng = new Random();
-    resetGame();
   }
 
   @Override
@@ -59,18 +68,16 @@ public class MainActivity extends AppCompatActivity {
     boolean handled = true;
     switch (item.getItemId()) {
       case R.id.play_one:
-        updateDisplay(game.play(), game.getWins(), game.getPlays(), game.getPercentage());
+        viewModel.playOne();
         break;
       case R.id.fast_forward:
-        running = true;
-        new Runner().start();
-        invalidateOptionsMenu();
+        viewModel.fastForward();
         break;
       case R.id.pause:
-        running = false;
+        viewModel.pause();
         break;
       case R.id.reset:
-        resetGame();
+        viewModel.reset();
         break;
       default:
         handled = super.onOptionsItemSelected(item);
@@ -78,59 +85,18 @@ public class MainActivity extends AppCompatActivity {
     return handled;
   }
 
-  private void updateDisplay(Round round, int wins, int plays, double percentage) {
+  private void updateRolls(Round round) {
     adapter.add(round);
+  }
+
+  private void updateTally(Game game) {
+    int wins = game.getWins();
+    int plays = game.getPlays();
+    double percentage = game.getPercentage();
     String winsLabel = getResources().getQuantityString(R.plurals.wins, wins);
     String playsLabel = getResources().getQuantityString(R.plurals.plays, plays);
     tally.setText(getString(R.string.tally_format,
         wins, plays, 100 * percentage, winsLabel, playsLabel));
-  }
-
-  private void resetGame() {
-    game = new Game(rng);
-    updateDisplay(null, 0, 0, 0);
-  }
-
-  private class Runner extends Thread {
-
-
-
-    @Override
-    public void run() {
-      while (running) {
-        Round round = game.play();
-        if (game.getPlays() % 500 == 0) {
-          int wins = game.getWins();
-          int plays = game.getPlays();
-          double percentage = game.getPercentage();
-          runOnUiThread(new Updater(round, wins, plays, percentage));
-        }
-      }
-      runOnUiThread(new Updater(game.play(), game.getWins(), game.getPlays(), game.getPercentage()));
-      invalidateOptionsMenu();
-    }
-
-  }
-
-  private class Updater implements Runnable {
-
-    private final Round round;
-    private final int wins;
-    private final int plays;
-    private final double percentage;
-
-    private Updater(Round round, int wins, int plays, double percentage) {
-      this.round = round;
-      this.wins = wins;
-      this.plays = plays;
-      this.percentage = percentage;
-    }
-
-    @Override
-    public void run() {
-      updateDisplay(round, wins, plays, percentage);
-    }
-
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/crapssimulator/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/crapssimulator/viewmodel/MainViewModel.java
@@ -12,15 +12,18 @@ import java.util.Random;
 
 public class MainViewModel extends AndroidViewModel {
 
-  private Random rng;
-  private MutableLiveData<Game> game;
-  private MutableLiveData<Round> round;
+  private final Random rng;
+  private final MutableLiveData<Game> game;
+  private final MutableLiveData<Round> round;
+  private final MutableLiveData<Boolean> running;
+  private Runner runner;
 
   public MainViewModel(@NonNull Application application) {
     super(application);
     rng = new SecureRandom();
     game = new MutableLiveData<>();
     round = new MutableLiveData<>();
+    running = new MutableLiveData<>();
     reset();
   }
 
@@ -32,23 +35,60 @@ public class MainViewModel extends AndroidViewModel {
     return round;
   }
 
-  public void play() {
-    round.setValue(game.getValue().play());
+  public LiveData<Boolean> isRunning() {
+    return running;
   }
 
-  public void play(int rounds) {
-    new Thread(() -> {
-      Round round = null;
-      for (int i = 0; i < rounds; i++) {
-        round = game.getValue().play();
-      }
-      MainViewModel.this.round.postValue(round);
-    }).start();
+  public void playOne() {
+    round.setValue(game.getValue().play());
+    game.setValue(game.getValue());
+  }
+
+  public void fastForward() {
+    running.setValue(true);
+    pause();
+    runner = new Runner();
+    runner.start();
+  }
+
+  public void pause() {
+    if (runner != null) {
+      runner.pause();
+    }
+    runner = null;
   }
 
   public void reset() {
     game.setValue(new Game(rng));
     round.setValue(null);
+    running.setValue(false);
+  }
+
+  private class Runner extends Thread {
+
+    private static final int ROUND_BATCH_SIZE = 1000;
+
+    private boolean running = true;
+
+    @Override
+    public void run() {
+      Game game = MainViewModel.this.game.getValue();
+      while (running) {
+        int limit = ROUND_BATCH_SIZE - game.getPlays() % ROUND_BATCH_SIZE;
+        Round round = null;
+        for (int i = 0; i < limit; i++) {
+          round = game.play();
+        }
+        MainViewModel.this.round.postValue(round);
+        MainViewModel.this.game.postValue(game);
+      }
+      MainViewModel.this.running.postValue(false);
+    }
+
+    public void pause() {
+      running = false;
+    }
+
   }
 
 }
